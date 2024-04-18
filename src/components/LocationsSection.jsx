400725f05477f305742c21a1ecd7b61b0caef75c
Fabian <baruafabian@gmail.com>
2024-04-18 16:43:28 -0400

    💄 Refactor LocationsSection component to display additional information for each location

--- src/components/LocationsSection.jsx
+++ src/components/LocationsSection.jsx
@@ -5,24 +5,17 @@ export const LocationsSection = ({ locations }) => {
     return (
         <section className="flex gap-3 justify-between flex-wrap">
             {
-                locations?.map(({ id, name }) => (
-                    <a key={id}
-                        href={`/lugares/${id}`}
-                        className=" hover:bg-default-100 transition-all relative border dark:border-0 border-default-400 bg-default-50  w-[10.08rem] sm:w-[12.2rem] flex flex-col  rounded-2xl p-4 text-xs  leading-4">
-
-                        <h3
-                            className="font-bold text-xl  text-default-900 "
-                            style={{ viewTransitionName: `planet-name-${id}` }}
-                        >{name}</h3>
-                        <div className=' absolute  bottom-1 right-1'>
-                            <ButtonAction border
-                                href={`/lugares/${id}`}>
-                                <InfoIcon />
-                            </ButtonAction>
-                        </div>
-                    </a>
+                locations?.map(({ id, name, type, dimension }) => (
+                    <div key={id} className="border dark:border-0 border-default-400 bg-default-50 w-[10.08rem] sm:w-[12.2rem] flex flex-col rounded-2xl p-4 text-xs leading-4">
+                        <h3 style={{ viewTransitionName: `planet-name-${id}` }} className="text-lg font-bold">{name}</h3>
+                        <p className="text-default-300 text-sm">Tipo: {type}</p>
+                        <p className="text-default-300 text-sm h-full mb-3">Dimensión: {dimension}</p>
+                        <ButtonAction size='sm' border href={`/lugares/${id}`}>
+                            <InfoIcon /> Ver más
+                        </ButtonAction>
+                    </div>
                 ))
             }
-        </section >
+        </section>
     );
 }
